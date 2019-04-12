91f2f2c055fb4d1c076c8d287ac65fac70ea422d
taiga <matsuotaiga.miraidenshi@gmail.com>
2019-04-12 17:27:10 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by 松尾大雅 on 2019/04/12.
+//  Copyright © 2019 松尾大雅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
